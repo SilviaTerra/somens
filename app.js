0147416fc312f52e0b09ce861de3d032475eda35
Charlie Wade <charlie@silviaterra.com>
2016-02-26 12:05:06 -0500

    slider jQuery

---
+++ app.js
@@ -0,0 +1,40 @@
+var main = function() {
+  $('.arrow-next').click(function() {
+    var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
+    
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+    
+    if(nextSlide.length === 0) {
+      nextSlide = $('.slide').first();
+    };
+    
+    if(nextDot.length === 0) {
+      nextDot = $('.dot').first();
+    }
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    nextSlide.fadeIn(600).addClass('active-slide');
+  })
+  $('.arrow-prev').click(function() {
+    var currentSlide = $('.active-slide');
+    var prevSlide = currentSlide.prev();
+    
+    var currentDot = $('.active-dot');
+    var prevDot = currentDot.prev();
+    
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot');
+    
+    if(prevSlide.length === 0) {
+      prevSlide = $('.slide').last();
+      prevDot = $('.dot').last();
+    };
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    prevSlide.fadeIn(600).addClass('active-slide');
+  })
+}
+
+$(document).ready(main)
